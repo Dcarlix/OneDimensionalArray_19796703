c263fd3c1c95fd8a622624b60cf9b8b9b385eb45
= <carlitosxd2006@gmail.com>
2025-02-21 09:00:44 -0600

    Subiendo la segunda parte del proyecto, donde va la otra pagina que puede usar el array

---
+++ OneDimensionArray_19796703/CategoryUseArraySessionVariables.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OneDimensionArray_19796703
+{
+	public partial class CategoryUseArraySessionVariables : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnLookup_Click(object sender, EventArgs e)
+        {
+			String[] strCategoryArray = new string[5];
+			strCategoryArray = Session["strCategoryArray"] as String[];
+			Int32 i32Index = Convert.ToInt32(txtIndex.Text);
+			lblCategory.Text = strCategoryArray[i32Index];
+        }
+    }
+}
